87568a630cf837ec1931bd97048c6800a17c8c59
Mustafa Özkara <mustafaozkara@outlook.fr>
2023-12-24 17:17:22 +0100

    Add many options to the calculator

--- js/script.js
+++ js/script.js
@@ -1,68 +1,231 @@
 function add (firstNumber, secondNumber) {   
-    return firstNumber + secondNumber;
+    return parseFloat(firstNumber) + parseFloat(secondNumber);
 }
 
 function subtract (firstNumber, secondNumber) {   
-    return firstNumber - secondNumber;
+    return parseFloat(firstNumber) - parseFloat(secondNumber);
 }
 
 function multiply (firstNumber, secondNumber) {   
-    return firstNumber * secondNumber;
+    return parseFloat(firstNumber) * parseFloat(secondNumber);
 }
 
-function divide (firstNumber, secondNumber) {   
-    return firstNumber / secondNumber;
+function divide (firstNumber, secondNumber) {  
+    if(secondNumber === "0") {
+        return alert("You can't divide by 0");
+    }
+    return parseFloat(firstNumber) / parseFloat(secondNumber);
 }
 
 function operate (firstNumber, secondNumber, operator)  {
     if (operator === "+") {
-        add(firstNumber, secondNumber);
-    } else if(operator === "+") {
-        subtract(firstNumber, secondNumber);
-    } else if(operator === "+") {
-        multiply(firstNumber, secondNumber);
-    } else {
-        divide(firstNumber, secondNumber);
+        return add(firstNumber, secondNumber);
+    } else if(operator === "-") {
+        return subtract(firstNumber, secondNumber);
+    } else if(operator === "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if(operator === "/") {
+        return divide(firstNumber, secondNumber);
     }
 }
 
+
+const buttons = document.querySelectorAll('.calculator-small-button');
 let displayAnswer = document.querySelector("#calculator-answer");
 let clearButton = document.querySelector(".clear-button");
 let deleteButton = document.querySelector(".delete-button");
-const buttons = document.querySelectorAll('.calculator-small-button')
-let number = "0";
-console.log(displayAnswer.textContent);
+let firstNumber = "0";
+let secondNumber = "0";
+let result = "0";
+let equalNumberDelete = "0";
+let resultStorage = 0;
+let equalCheck = 0;
+let operatorCheck;
+let doubleEqualCheck;
+
+
+
 buttons.forEach(btn => {
     btn.addEventListener('click', function () {
-        if (number.includes(".") && btn.textContent === ".") {
+        if (btn.textContent === "=") {
+            if (btn.textContent === "=" && doubleEqualCheck === "=") {
+                return;
+            } else if (operatorCheck.length === 0) {
+                displayAnswer.textContent = firstNumber;
+                return;
+            } else if (secondNumber === 0 && operatorCheck === "/") {
+                return alert("You can't divide by0");
+            }
+             doubleEqualCheck = "=";
+            result = operate(firstNumber, secondNumber, operatorCheck);
+            displayAnswer.textContent = result;
+            displayAnswer.style.fontSize = "46px";
+            firstNumber = result;
+            equalNumberDelete = result;
+            equalCheck++;
+            equalDeleteCounter = 0;
+            secondNumber = "0";
+            operatorCheck = "";
+            result = 0;
             return;
-        } else if (btn.textContent === ".") {
-            number = number + btn.textContent;
-            displayAnswer.textContent = number;
+        }
+
+
+        if (equalCheck > 0) {
+            doubleEqualCheck = "";
+            if (btn.textContent === "+" || btn.textContent === "-" || btn.textContent === "*" || btn.textContent === "/") {
+                operatorCheck = btn.textContent;
+            
+            if (equalDeleteCounter > 0) {
+                firstNumber = equalNumberDelete;
+            }
+
+            firstNumber = firstNumber + btn.textContent;
+            if (firstNumber.includes("++") && btn.textContent === "+" || firstNumber.includes("--") && btn.textContent === "-" || firstNumber.includes("**") && btn.textContent === "*" || firstNumber.includes("//") && btn.textContent === "/") {
+                console.log("hey");
+                firstNumber = firstNumber.splice(0, -1);
+                displayAnswer.textContent = firstNumber;
+                displayAnswer.style.fontSize = "46px";
+                return;
+            } 
+            displayAnswer.textContent = firstNumber;
             displayAnswer.style.fontSize = "46px";
             return;
-        } else if (btn.textContent === "0" && number === "0") {
+        } 
+
+
+
+            if (operatorCheck === "+" || operatorCheck === "-" || operatorCheck === "*" || operatorCheck === "/") {
+                displayAnswer.textContent = "";
+    
+                if (secondNumber.includes(".") && btn.textContent === ".") {
+                    return;
+                } else if (btn.textContent === ".") {
+                    secondNumber = secondNumber + btn.textContent;
+                    displayAnswer.textContent = secondNumber;
+                    displayAnswer.style.fontSize = "46px";
+                    return;
+                } else if (secondNumber === "0" ) {
+                    secondNumber = btn.textContent;
+                    displayAnswer.textContent = "";
+                    displayAnswer.textContent = btn.textContent;
+                    displayAnswer.style.fontSize = "46px";
+                    return;
+                }
+                
+                if (btn.textContent === "+" && operatorCheck === "+") {
+                    btn.textContent === "";
+                    return;
+                }
+
+                secondNumber = secondNumber + btn.textContent;
+                if (btn.textContent === "+" || btn.textContent === "-" || btn.textContent === "*" || btn.textContent === "/") {
+                    secondNumber = secondNumber.slice(0, -1);     
+                    displayAnswer.textContent = secondNumber;
+                    displayAnswer.style.fontSize ="46px"; 
+                    return;
+                }
+                displayAnswer.textContent = secondNumber;
+                displayAnswer.style.fontSize = "46px";
+                return;
+            }
+        }
+
+
+
+
+
+
+        if (operatorCheck === "+" || operatorCheck === "-" || operatorCheck === "*" || operatorCheck === "/") {
+            displayAnswer.textContent = "";
+
+            if (secondNumber.includes(".") && btn.textContent === ".") {
+                return;
+            } else if (btn.textContent === ".") {
+                secondNumber = secondNumber + btn.textContent;
+                displayAnswer.textContent = secondNumber;
+                displayAnswer.style.fontSize = "46px";
+                return;
+            }  else if (secondNumber === "0" ) {
+                if (btn.textContent === "+" && operatorCheck === "+" || btn.textContent ==="-" && operatorCheck === "-" || btn.textContent ==="*" && operatorCheck === "*" || btn.textContent ==="/" && operatorCheck === "/") {
+                    btn.textContent === "";
+                    displayAnswer.textContent = firstNumber;
+                    displayAnswer.style.fontSize = "46px";
+                    return;
+                } 
+                secondNumber = btn.textContent;
+                displayAnswer.textContent = "";
+                displayAnswer.textContent = btn.textContent;
+                displayAnswer.style.fontSize = "46px";
+                return;
+            }
+
+            secondNumber = secondNumber + btn.textContent;
+            if (btn.textContent === "+" || btn.textContent === "-" || btn.textContent === "*" || btn.textContent === "/") {
+                secondNumber = secondNumber.slice(0, -1);     
+                displayAnswer.textContent = secondNumber;
+                displayAnswer.style.fontSize ="46px"; 
+                return;
+            }
+            displayAnswer.textContent = secondNumber;
+            displayAnswer.style.fontSize = "46px";
             return;
-        } else if (number === "0" ) {
-            number = btn.textContent;
+        }
+
+        firstNumber = firstNumber.toString();
+        if (firstNumber.includes(".") && btn.textContent === ".") {
+            return;
+        } else if (btn.textContent === ".") {
+            firstNumber = firstNumber + btn.textContent;
+            displayAnswer.textContent = firstNumber;
+            displayAnswer.style.fontSize = "46px";
+            return;
+        } else if (btn.textContent === "0" && firstNumber === "0") {
+            return;
+        } else if (firstNumber === "0" ) {
+            firstNumber = btn.textContent;
             displayAnswer.textContent = "";
             displayAnswer.textContent = btn.textContent;
             displayAnswer.style.fontSize = "46px";
             return;
         }
 
-        number = number + btn.textContent;
-        displayAnswer.textContent = number;
-        displayAnswer.style.fontSize = "46px";
+         if (btn.textContent === "+" || btn.textContent === "-" || btn.textContent === "*" || btn.textContent === "/") {
+            operatorCheck = btn.textContent;
+        } 
+
+        firstNumber = firstNumber + btn.textContent;
+        displayAnswer.textContent = firstNumber;
+        displayAnswer.style.fontSize = "46px"; 
     });
 }); 
 
+
+
+
 clearButton.addEventListener("click", function() {
     displayAnswer.textContent = 0;
-    number = "0";
+    firstNumber = "0";
+    secondNumber = "0";
+    equalNumberDelete = "0";
 });
 
+
+
 deleteButton.addEventListener("click", function() {
-      number = number.slice(0, -1); 
-      displayAnswer.textContent = number;
+    if (operatorCheck === "+" || operatorCheck === "-" || operatorCheck === "*" ||operatorCheck === "/") {
+        secondNumber = secondNumber.slice(0, -1); 
+        secondNumber = parseFloat(secondNumber);
+        displayAnswer.textContent = secondNumber;
+        return;
+    } else if (equalCheck > 0) {
+        equalNumberDelete = equalNumberDelete.toString();
+        equalNumberDelete = equalNumberDelete.slice(0, -1);
+        displayAnswer.textContent = equalNumberDelete;
+        equalDeleteCounter++;  
+    } 
+      firstNumber = firstNumber.slice(0, -1); 
+      displayAnswer.textContent = firstNumber;
 });
+
+
